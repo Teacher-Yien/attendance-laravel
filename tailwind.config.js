3297b1b23892e7935ac86dd4e23018d13e719dda
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2025-03-19 17:42:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.tsx",
+    "./resources/js/**/*.ts",
+    "./resources/js/**/*.jsx",
+    "./resources/js/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
